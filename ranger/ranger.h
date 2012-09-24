b30accd34fb0638ac4f1e53f6aaa9f392dd816ce
Sebastian Jeltsch <sjeltsch@kip.uni-heidelberg.de>
2012-09-24 18:33:18 +0200

    add banner

--- ranger/ranger.h
+++ ranger/ranger.h
@@ -1,5 +1,8 @@
 #pragma once
 
+// Copyright (c) 2012, Sebastian Jeltsch (sjeltsch@kip.uni-heidelberg.de)
+// Distributed under the terms of the GPLv2 or newer
+
 #include <limits>
 #include <type_traits>
 
