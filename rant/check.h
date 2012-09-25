cce3f15e828339d6fda770851299d06bd1fffa3b
Sebastian Jeltsch <sjeltsch@kip.uni-heidelberg.de>
2012-09-25 17:30:54 +0200

    compile switch for expressive exceptions

--- rant/check.h
+++ rant/check.h
@@ -5,18 +5,26 @@
 
 #include <stdexcept>
 #include <string>
-#include <typeinfo>
 
 #include "rant/util.h"
+
+#ifdef RANT_LIGHTWEIGHT_EXCEPTIONS
+#define RANT_UNDERFLOW_ERROR(VAL, MIN) std::underflow_error("")
+#define RANT_OVERFLOW_ERROR(VAL, MAX)  std::overflow_error("")
+#else
+#define RANT_UNDERFLOW_ERROR(VAL, MIN) rant::underflow(VAL, MIN)
+#define RANT_OVERFLOW_ERROR(VAL, MAX)  rant::overflow(VAL, MAX)
+#endif // RANT_LIGHTWEIGHT_EXCEPTIONS
+
+
+namespace rant {
 
 template<typename T>
 inline
 std::underflow_error
 underflow(T val, T min)
 {
-	std::string s("type(");
-	s += typeid(T).name();
-	s += ") overflow error: ";
+	std::string s("underflow error: ");
 	s += std::to_string(val) + " < min(";
 	s += std::to_string(min) + ")";
 	return std::underflow_error(s);
@@ -27,15 +35,11 @@ inline
 std::overflow_error
 overflow(T val, T max)
 {
-	std::string s("type(");
-	s += typeid(T).name();
-	s += ") overflow error: ";
+	std::string s("overflow error: ");
 	s += std::to_string(val) + " >= max(";
 	s += std::to_string(max) + ")";
 	return std::overflow_error(s);
 }
-
-namespace rant {
 
 template<typename T, typename Max, typename Min>
 typename std::enable_if<std::is_integral<T>::value>::type
@@ -44,9 +48,9 @@ check(T val)
 	static_assert(Max::value >= Min::value, "Max must be >= Min");
 
 	if (!(val >= Min::value))
-		throw underflow(val, Min::value);
+		throw RANT_UNDERFLOW_ERROR(val, Min::value);
 	else if (!(val < Max::value))
-		throw overflow(val, Max::value);
+		throw RANT_OVERFLOW_ERROR(val, Max::value);
 }
 
 template<typename T, typename Max, typename Min>
@@ -57,9 +61,12 @@ check(T val)
 		"Max must be >= Min");
 
 	if (!(val >= static_cast<T>(Min::num) / Min::den))
-		throw underflow(val, static_cast<T>(Min::num) / Min::den);
+		throw RANT_UNDERFLOW_ERROR(val, static_cast<T>(Min::num) / Min::den);
 	else if (!(val < static_cast<T>(Max::num) / Max::den))
-		throw overflow(val, static_cast<T>(Max::num) / Max::den);
+		throw RANT_OVERFLOW_ERROR(val, static_cast<T>(Max::num) / Max::den);
 }
 
 } // namespace rant
+
+#undef RANT_UNDERFLOW_ERROR
+#undef RANT_OVERFLOW_ERROR
