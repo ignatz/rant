7c58d64e42f31f10063cb20d2b69d14451466a57
Sebastian Jeltsch <sjeltsch@kip.uni-heidelberg.de>
2012-09-27 19:40:47 +0200

    reduce redundancy

--- rant/impl.h
+++ rant/impl.h
@@ -11,6 +11,8 @@
 #include "rant/util.h"
 #include "rant/check.h"
 
+#define RANT_VALUE __val
+
 #define RANT_ARITHMETIC_OPS                                   \
 	RANT_OPERATOR_BINARY(self, +)                             \
 	RANT_OPERATOR_BINARY(self, -)                             \
@@ -25,70 +27,85 @@
 	RANT_OPERATOR_ASSIGNMENT(*)                               \
 	RANT_OPERATOR_ASSIGNMENT(/)
 
+#ifndef RANT_DISABLE_SERIALIZATION
 #define RANT_SERIALIZATION                                    \
+private:                                                      \
 	friend class boost::serialization::access;                \
 	template<typename Archiver>                               \
 	void serialize(Archiver& ar, unsigned int)                \
 	{                                                         \
-		ar & BOOST_SERIALIZATION_NVP(_val);                   \
+		ar & BOOST_SERIALIZATION_NVP(RANT_VALUE);             \
 	}
+#else
+#define RANT_SERIALIZATION
+#endif // RANT_DISABLE_SERIALIZATION
 
-#define RANT_BASE                                             \
+#define RANT_OPERATORS(...)                                   \
 protected:                                                    \
-	static_assert(value<T, Max>() >= value<T, Min>(),         \
-				  "Max must be >= Min");                      \
-                                                              \
-	T _val;                                                   \
+	T RANT_VALUE;                                             \
 public:                                                       \
 	typedef range<T, Max, Min> self;                          \
                                                               \
-	range(T val = T()) : _val(val)                            \
+	range(T v = T()) : RANT_VALUE(v)                          \
 	{                                                         \
-		check<T, Max, Min>(val);                              \
+		check<T, Max, Min>(RANT_VALUE);                       \
 	}                                                         \
                                                               \
 	inline operator T () const noexcept                       \
 	{                                                         \
-		return _val;                                          \
+		return RANT_VALUE;                                    \
 	}                                                         \
                                                               \
-	static constexpr T min()                                  \
+	static constexpr T min() noexcept                         \
 	{                                                         \
 		return value<T, Min>();                               \
 	}                                                         \
                                                               \
-	static constexpr T max()                                  \
+	static constexpr T max() noexcept                         \
 	{                                                         \
 		return value<T, Max>();                               \
+	}                                                         \
+	static_assert(max() >= min(), "Max must be >= Min");      \
+                                                              \
+	RANT_ARITHMETIC_OPS                                       \
+	RANT_ASSIGNMENT_OPS                                       \
+	__VA_ARGS__
+
+#define RANT_OPERATOR_UNARY(RET, OP)                          \
+	inline RET operator OP () const                           \
+	{                                                         \
+		return RET(OP RANT_VALUE);                            \
 	}
 
 #define RANT_OPERATOR_BINARY(RET, OP)                         \
-	inline                                                    \
-	RET operator OP (self x) const                            \
+	inline RET operator OP (self x) const                     \
 	{                                                         \
-		return RET(this->_val OP x);                          \
-	}
-
-#define RANT_OPERATOR_UNARY(RET, OP)                          \
-	inline                                                    \
-	RET operator OP () const                                  \
-	{                                                         \
-		return RET(OP this->_val);                            \
+		return RET(RANT_VALUE OP x);                          \
 	}
 
 #define RANT_OPERATOR_ASSIGNMENT(OP)                          \
-	inline                                                    \
-	self& operator OP##= (self x)                             \
+	inline self& operator OP##= (self x)                      \
 	{                                                         \
-		check<T, Max, Min>(this->_val OP x);                  \
-		this->_val OP##= x;                                   \
-		return *static_cast<self*>(this);                     \
+		check<T, Max, Min>(RANT_VALUE OP x);                  \
+		RANT_VALUE OP##= x;                                   \
+		return *this;                                         \
+	}
+
+#define RANT_OPERATOR_INCREMENTAL(OP)                         \
+	inline self& operator OP##OP ()                           \
+	{                                                         \
+		return *this OP##= 1;                                 \
+	}                                                         \
+	inline self operator OP##OP (int)                         \
+	{                                                         \
+		self t(*this);                                        \
+		*this OP##= 1;                                        \
+		return t;                                             \
 	}
 
 #define RANT_OPERATOR_COMPARE_FF(RET, OP)                     \
 	template<typename T, typename Max, typename Min>          \
-	inline                                                    \
-	RET operator OP (                                         \
+	inline RET operator OP (                                  \
 		range<T, Max, Min> a,                                 \
 		range<T, Max, Min> b) noexcept                        \
 	{                                                         \
@@ -106,7 +123,6 @@ template<typename T, typename Max, typename Min>
 class range<T, Max, Min,
 	typename std::enable_if<std::is_integral<T>::value>::type>
 {
-private:
 	static_assert(std::is_integral<T>::value,
 				  "T must be integral type");
 	static_assert(is_integral_constant<Max>::value,
@@ -114,58 +130,28 @@ private:
 	static_assert(is_integral_constant<Min>::value,
 				  "Min must be std::integral_constant type");
 
-public:
-	RANT_BASE
-	RANT_ARITHMETIC_OPS
-	RANT_ASSIGNMENT_OPS
+	RANT_OPERATORS
+	(
+		// integral arithmetic operations
+		RANT_OPERATOR_BINARY(self, %)
+		RANT_OPERATOR_ASSIGNMENT(%)
+		RANT_OPERATOR_INCREMENTAL(+)
+		RANT_OPERATOR_INCREMENTAL(-)
 
-	// arithmetic operations
-	RANT_OPERATOR_BINARY(self, %)                                   \
-	RANT_OPERATOR_ASSIGNMENT(%)                               \
+		// integral logical operations
+		RANT_OPERATOR_UNARY(bool, !)
+		RANT_OPERATOR_BINARY(bool, &&)
+		RANT_OPERATOR_BINARY(bool, ||)
 
-	// logical operations
-	RANT_OPERATOR_UNARY(bool, !)
-	RANT_OPERATOR_BINARY(bool, &&)
-	RANT_OPERATOR_BINARY(bool, ||)
-
-	// bitwise operations
-	RANT_OPERATOR_UNARY(self, ~)
-	RANT_OPERATOR_BINARY(self, &)
-	RANT_OPERATOR_BINARY(self, |)
-	RANT_OPERATOR_BINARY(self, ^)
-	RANT_OPERATOR_BINARY(self, <<)
-	RANT_OPERATOR_BINARY(self, >>)
-
-	// prefix ++/-- operators
-	self& operator++ ()
-	{
-		return *this += 1;
-	}
-
-	self& operator-- ()
-	{
-		return *this -= 1;
-	}
-
-	// postfix ++/-- operators
-	self operator++ (int)
-	{
-		self t(*this);
-		*this += 1;
-		return t;
-	}
-
-	self operator-- (int)
-	{
-		self t(*this);
-		*this -= 1;
-		return t;
-	}
-
-#ifndef RANT_DISABLE_SERIALIZATION
-private:
+		// integral bitwise operations
+		RANT_OPERATOR_UNARY(self, ~)
+		RANT_OPERATOR_BINARY(self, &)
+		RANT_OPERATOR_BINARY(self, |)
+		RANT_OPERATOR_BINARY(self, ^)
+		RANT_OPERATOR_BINARY(self, <<)
+		RANT_OPERATOR_BINARY(self, >>)
+	)
 	RANT_SERIALIZATION
-#endif // RANT_DISABLE_SERIALIZATION
 };
 
 
@@ -173,7 +159,6 @@ template<typename T, typename Max, typename Min>
 class range<T, Max, Min,
 	typename std::enable_if<std::is_floating_point<T>::value>::type>
 {
-private:
 	static_assert(std::is_floating_point<T>::value,
 				  "T must be floating point type");
 	static_assert(is_ratio<Max>::value,
@@ -181,15 +166,8 @@ private:
 	static_assert(is_ratio<Min>::value,
 				  "Min must be std::ratio type");
 
-public:
-	RANT_BASE
-	RANT_ARITHMETIC_OPS
-	RANT_ASSIGNMENT_OPS
-
-#ifndef RANT_DISABLE_SERIALIZATION
-private:
+	RANT_OPERATORS()
 	RANT_SERIALIZATION
-#endif // RANT_DISABLE_SERIALIZATION
 };
 
 
@@ -206,7 +184,7 @@ RANT_OPERATOR_COMPARE_FF(bool, >=)
 #undef RANT_ARITHMETIC_OPS
 #undef RANT_ASSIGNMENT_OPS
 #undef RANT_SERIALIZATION
-#undef RANT_BASE
+#undef RANT_OPERATORS
 #undef RANT_OPERATOR_BINARY
 #undef RANT_OPERATOR_UNARY
 #undef RANT_OPERATOR_ASSIGNMENT
